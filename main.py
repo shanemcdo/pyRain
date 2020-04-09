de91f1851bdf42da9a7a37c4a01bd357cc01172a
Shane McDonough <shanemcdman@gmail.com>
2020-04-08 20:57:48 -0700

    Hide the cursor and end at the bottom

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
+import cursor
 from Trail import Trail
 
 
-
 def main():
+	cursor.hide()
 	width, height = os.get_terminal_size()
 	trail = Trail(10,5)
 	trail.draw()
+	Trail.gotoxy(1,height-1)
+	cursor.show()
 
 if __name__ == '__main__':
 	main()
