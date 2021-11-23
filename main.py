26f2afc95c89363e6546c5a8e22b9fdb79d9ff99
Shane McDonough <shanemcdman@gmail.com>
2021-11-23 13:51:59 -0800

    Make changes for mac

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
+#! /usr/bin/env python3
 import os
 import random
 import cursor
 from Trail import Trail
-from msvcrt import kbhit, getch
-
-def kbin():
-    global running
-    if kbhit():
-        ch = getch().decode('utf-8')
-        if ch == 'q':
-            running = False
 
 def main():
-    global running
-    cursor.hide()
-    os.system('mode con: cols=30 lines=15')
-    os.system("cls")
-    width, height = os.get_terminal_size()
-    trails = [Trail(width, height, x, random.randrange(1, height)) for x in range(1, width + 1)]
-    running = True
-    while running:
-        for trail in trails:
-            trail.draw()
-            trail.update()
-        kbin()
-    Trail.gotoxy(1, height-1)
-    cursor.show()
-    os.system('mode con: cols=165 lines=42')
+    try:
+        cursor.hide()
+        os.system('clear')
+        width, height = os.get_terminal_size()
+        trails = [Trail(width, height, x, random.randrange(1, height)) for x in range(1, width + 1)]
+        while True:
+            for trail in trails:
+                trail.draw()
+                trail.update()
+        Trail.gotoxy(1, height-1)
+    except KeyboardInterrupt:
+        os.system('clear')
+        cursor.show()
 
 if __name__ == '__main__':
     main()
